93fa801311c6957115ca16846a632f105fbd22c1
Anshuman202 <asanshuman2@gmail.com>
2023-01-01 21:13:51 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LineComparison
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line comparison program");
+            LineLenth obj = new LineLenth();
+            obj.Lenth();
+            /*TwoLineLength obj = new TwoLineLength();
+            obj.Twoline();*/
+            /*EndpointLength obj = new EndpointLength();
+            obj.Point();*/
+
+
+
+
+
+        }
+ 
+    
+    
+    }
+}
